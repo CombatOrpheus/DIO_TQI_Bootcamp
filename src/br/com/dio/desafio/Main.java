682030384366c38cb93217352f9cdaeda1893cbc
Gabriel Santos Oliveira <gsoliveira@localhost.localdomain>
2023-04-15 16:27:16 -0300

    More classes and some fixes

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -7,14 +7,14 @@ import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Curso curso1 = new Curso("curso java", "descricao curso java", 8);
-		Curso curso2 = new Curso("curso js", "descricao curso js", 4);
-		Mentoria mentoria = new Mentoria("mentoria java", "descricao mentoria java", LocalDate.now());
+    public static void main(String[] args) {
+	Curso curso1 = new Curso("curso java", "descricao curso java", 8);
+	Curso curso2 = new Curso("curso js", "descricao curso js", 4);
+	Mentoria mentoria = new Mentoria("mentoria java", "descricao mentoria java", LocalDate.now());
 
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria);
-	}
+	System.out.println(curso1);
+	System.out.println(curso2);
+	System.out.println(mentoria);
+    }
 
 }
---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,46 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(5);
+    private Set<Dev> inscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Set<Dev> getInscritos() {
+        return inscritos;
+    }
+    public void setInscritos(Set<Dev> inscritos) {
+        this.inscritos = inscritos;
+    }
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+}
